5be165d65f77b972e5e2deebab183297dabfb91c
йцу <>
2024-09-04 09:16:51 +0300

    AutoCommit_4 сентября 2024 г. 9:16:51_SibNout2023

--- Краткое руководство/HelloWorld/src/HelloWorld/HelloWorldClass.java
+++ Краткое руководство/HelloWorld/src/HelloWorld/HelloWorldClass.java
@@ -4,6 +4,7 @@ public class HelloWorldClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("C:\\D\\GitHub\\GitHubOll\\HowTo_JAVA\\Краткое руководство\\HelloWorld");
 		System.out.println("HelloWorld");
 
 	}
